5f0e1a2d7eae9d889a78914970f83f1f2fa9edd9
Pedro Bittencourt <pedromonteirobittencourt@gmail.com>
2023-08-12 18:02:06 -0300

    Implementing possible King moves

--- application/UI.java
+++ application/UI.java
@@ -58,7 +58,7 @@ public class UI {
         System.out.println("  a b c d e f g h");
     }
 
-        public static void printBoard(ChessPiece[][] pieces, boolean[][] possibleMoves){
+    public static void printBoard(ChessPiece[][] pieces, boolean[][] possibleMoves){
         for (int i = 0; i<pieces.length; i++){
             System.out.print((8 - i) + " ");
             for (int j = 0; j<pieces.length; j++){
